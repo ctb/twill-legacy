8f750222c7c8daeab7cc7ce35edf7afd70961831
Christoph Zwerschke <cito@online.de>
2020-01-19 19:48:56 +0100

    Simplify WSGI intercept tests
    Remove obscure WrapperApp test.
    This did not even work with the simple app,
    it had nothing to do with the write fn.

--- tests/test_wsgi_intercept.py
+++ tests/test_wsgi_intercept.py
@@ -16,88 +16,26 @@ def teardown_module():
     requests_intercept.uninstall()
 
 
-_app_was_hit = False
+app_was_hit = set()
 
 
-def success():
-    return _app_was_hit
-
-
-def simple_app(environ, start_response):
+def simple_app(_environ, start_response):
     """Simplest possible application object"""
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-
-    global _app_was_hit
-    _app_was_hit = True
-
+    app_was_hit.add('simple_app')
     return [b'WSGI intercept successful!\n']
 
 
-def test_intercept():
-    global _app_was_hit
-    _app_was_hit = False
-
-    add_wsgi_intercept('localhost', 80, lambda: simple_app)
-    assert not _app_was_hit
-    commands.go('http://localhost:80/')
-    commands.show()
-    commands.find("WSGI intercept successful")
-    assert _app_was_hit
-    remove_wsgi_intercept('localhost', 80)
-
-
-def write_app(environ, start_response):
+def write_app(_environ, start_response):
     """Test the 'write_fn' legacy stuff."""
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     write_fn = start_response(status, response_headers)
-
-    global _app_was_hit
-    _app_was_hit = True
-
-    write_fn('WSGI intercept successful!\n')
+    app_was_hit.add('write_app')
+    write_fn(b'WSGI intercept successful!\n')
     return []
-
-
-class WrapperApp:
-    """Test some tricky generator stuff in wsgi_intercept."""
-
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        generator = self.app(environ, start_response)
-
-        for i in generator:
-            yield i
-
-
-@pytest.mark.skip("broken since wsgi_intercept 0.10.1")
-def test_wrapper_intercept():
-    """Test a tricky wsgi_intercept interaction.
-
-    This tests a tricky wsgi_intercept interaction between the 'write' fn
-    passed back from the start_response function in WSGI, and the generator
-    data yielded from the initial app call.  See wsgi_intercept.py, section
-    containing 'generator_data', for more info.
-
-    This test had worked in all wsgi_intercept versions before 0.10.1.
-    In later versions, this seems to be broken again.
-    This should be fixed in wsgi_intercept, if it is a real problem.
-    """
-    global _app_was_hit
-    _app_was_hit = False
-
-    wrap_app = WrapperApp(write_app)
-
-    add_wsgi_intercept('localhost', 80, lambda: wrap_app)
-    assert not _app_was_hit
-    commands.go('http://localhost:80/')
-    commands.find("WSGI intercept successful")
-    assert _app_was_hit
-    remove_wsgi_intercept('localhost', 80)
 
 
 class IteratorApp:
@@ -119,6 +57,28 @@ class IteratorApp:
         return next(self._iter)
 
     next = __next__  # for Python 2
+
+
+def test_intercept():
+    app_was_hit.clear()
+    add_wsgi_intercept('localhost', 8080, lambda: simple_app)
+    assert not app_was_hit
+    commands.go('http://localhost:8080/')
+    commands.show()
+    commands.find("WSGI intercept successful")
+    assert 'simple_app' in app_was_hit
+    remove_wsgi_intercept('localhost', 8080)
+
+
+def test_write_intercept():
+    app_was_hit.clear()
+    add_wsgi_intercept('localhost', 8080, lambda: write_app)
+    assert not app_was_hit
+    commands.go('http://localhost:8080/')
+    commands.show()
+    commands.find("WSGI intercept successful")
+    assert 'write_app' in app_was_hit
+    remove_wsgi_intercept('localhost', 8080)
 
 
 def test_iter_stuff():
