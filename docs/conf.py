1fd5e66653570d44bf0a43b4ee634ab113aab46f
Christoph Zwerschke <cito@online.de>
2023-11-02 21:56:12 +0100

    Fix path when creating docs

--- docs/conf.py
+++ docs/conf.py
@@ -7,25 +7,24 @@ https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # -- Project information -----------------------------------------------------
 
-from twill import __version__
+project = "twill"
+author = "C. Titus Brown, Ben R. Taylor, Christoph Zwerschke et al"
+copyright = "2023, " + author
 
-project = 'twill'
-copyright = '2023, C. Titus Brown, Ben R. Taylor, Christoph Zwerschke et al'
-author = 'C. Titus Brown, Ben R. Taylor, Christoph Zwerschke et al'
+from twill import __version__
 
 # The full version, including alpha/beta/rc tags
 version = release = __version__
+
+language = "en"
 
 
 # -- General configuration ---------------------------------------------------
@@ -41,7 +40,7 @@ extensions = []
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -49,7 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
